47ea674af889fe8404b8376e862e917910eab99c
Alexandr Litovchenko <zedlaa@gmail.com>
2016-07-10 22:14:35 +0300

    Soma init sql

--- data/init.sql
+++ data/init.sql
@@ -7,8 +7,19 @@ CREATE TABLE pages (
   content TEXT
 );
 
+DROP TABLE IF EXISTS page_history;
+CREATE TABLE page_history (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  page_id INTEGER,
+  timestamp INTEGER,
+  title TEXT,
+  content TEXT
+);
+
 DROP TABLE IF EXISTS options;
 CREATE TABLE options (
   name TEXT PRIMARY KEY,
   value TEXT
 );
+
+
