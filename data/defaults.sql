fcc9302b321ec3d321697ff5cdfdf1e65c4b4afd
Alexandr Litovchenko <zedlaa@gmail.com>
2016-07-10 22:15:11 +0300

    Remove old code

--- data/defaults.sql
+++
@@ -1,7 +0,0 @@
-INSERT OR IGNORE INTO pages (lang, path, title, content) VALUES
-  ('ru', 'default_main', 'Welcome to Wikitone', ''),
-  ('ru', 'help', 'Wikitone help', '');
-
-INSERT OR IGNORE INTO options (name, value) VALUES
-  ('main_page', 'default_main'),
-  ('default_lang', 'ru');
